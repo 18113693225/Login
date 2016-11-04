2166285c15f4e132325d7c719316dfe45761c014
wilson <309150945@qq.com>
2016-11-04 16:29:48 +0800

    first commit

---
+++ app/src/main/java/com/lj/apps/login/EditTextClearTools.java
@@ -0,0 +1,51 @@
+package com.lj.apps.login;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+/**
+ * Created by SEELE on 2016-11-04.
+ */
+
+public class EditTextClearTools {
+
+    public static void addClearListener(final EditText e1, final ImageView m1) {
+
+        e1.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String s1 = s + "";
+                if (s.length() > 0) {
+                    m1.setVisibility(View.VISIBLE);
+                } else {
+                    m1.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+        m1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                e1.setText("");
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/lj/apps/login/MainActivity.java
@@ -0,0 +1,28 @@
+package com.lj.apps.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText e1, e2;
+    ImageView m1, m2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        init();
+    }
+
+    private void init() {
+        e1 = (EditText) findViewById(R.id.phone_number);
+        e2 = (EditText) findViewById(R.id.password);
+        m1 = (ImageView) findViewById(R.id.del_phone_number);
+        m2 = (ImageView) findViewById(R.id.del_password);
+        EditTextClearTools.addClearListener(e1, m1);
+        EditTextClearTools.addClearListener(e2, m2);
+
+    }
+}
