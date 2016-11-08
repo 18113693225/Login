e7a17576f039bc669e2238554570bd6deb94e8cc
wilson <309150945@qq.com>
2016-11-08 11:44:13 +0800

    test md progressBar

--- app/src/main/java/com/lj/apps/login/MainActivity.java
+++ app/src/main/java/com/lj/apps/login/MainActivity.java
@@ -4,16 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import me.zhanghai.android.materialprogressbar.IndeterminateProgressDrawable;
 
 public class MainActivity extends AppCompatActivity {
     EditText e1, e2;
     ImageView m1, m2;
 
+    ProgressBar indeterminateProgressLarge;
+    ProgressBar indeterminateProgress;
+    ProgressBar indeterminateProgressSmall;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        init();
+        setContentView(R.layout.custom_progress_bar);
+        // init();
+        indeterminateProgressLarge = (ProgressBar) findViewById(R.id.indeterminate_progress_large_library);
+
+        indeterminateProgress = (ProgressBar) findViewById(R.id.indeterminate_progress_library);
+
+        indeterminateProgressSmall = (ProgressBar) findViewById(R.id.indeterminate_progress_small_library);
+
+        indeterminateProgressLarge.setIndeterminateDrawable(
+                new IndeterminateProgressDrawable(this));
+        indeterminateProgress.setIndeterminateDrawable(new IndeterminateProgressDrawable(this));
+        indeterminateProgressSmall.setIndeterminateDrawable(
+                new IndeterminateProgressDrawable(this));
+
     }
 
     private void init() {
