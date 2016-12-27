096f67ccddf27869bcb188804562ce3a75d7470a
lijun <309150945@qq.com>
2016-12-27 16:18:09 +0800

    基本功能实现了

--- app/src/main/java/com/lj/apps/login/ui/activity/TakePhotosActivity.java
+++ app/src/main/java/com/lj/apps/login/ui/activity/TakePhotosActivity.java
@@ -1,15 +1,30 @@
 package com.lj.apps.login.ui.activity;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Message;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
 import com.lj.apps.login.R;
 import com.lj.apps.login.ui.widget.ImageRecyclerView;
+import com.lj.apps.login.utils.Constant;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,10 +39,15 @@ public class TakePhotosActivity extends BaseActivity implements ImageRecyclerVie
     ImageRecyclerView img_rv;
     @BindView(R.id.img)
     ImageView img;
+    @BindView(R.id.saveView)
+    RelativeLayout mSaveArea;
     String[] image = {"http://img.51ztzj.com/upload/image/20140401/sj2014040305_279x419.jpg", "http://img.51ztzj.com/upload/image/20140916/sj201409161014_279x419.jpg"
             , "http://img.51ztzj.com/upload/image/20141111/sj201411101025_279x419.jpg", "http://img.51ztzj.com/upload/image/20141225/sj201412251021_279x419.jpg", "http://img2.imgtn.bdimg.com/it/u=1922929249,102637201&fm=23&gp=0.jpg"};
     List<String> Img = new ArrayList<>();
-
+    private Bitmap saveBitmap;
+    private int mCutTop;
+    private int mCutLeft;
+    private int[] mSavePositions = new int[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +65,18 @@ public class TakePhotosActivity extends BaseActivity implements ImageRecyclerVie
         img_rv.setData(Img, this);
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if (hasFocus) {
+            mSaveArea.getLocationOnScreen(mSavePositions);
+            mCutLeft = mSavePositions[0];
+            mCutTop = mSavePositions[1];
+        }
+    }
+
     @OnClick(R.id.save_bt)
     public void onClick(View view) {
-
+        screenshot();
     }
 
 
@@ -56,4 +85,51 @@ public class TakePhotosActivity extends BaseActivity implements ImageRecyclerVie
         Glide.with(this).load(data).error(R.mipmap.ic_launcher).into(img);
     }
 
+    private void screenshot() {
+        // 获取屏幕
+        View dView = getWindow().getDecorView();
+        dView.setDrawingCacheEnabled(true);
+        dView.buildDrawingCache();
+        Bitmap bmp = dView.getDrawingCache();
+        if (bmp != null) {
+            try {
+                //二次截图
+                saveBitmap = Bitmap.createBitmap(mSaveArea.getWidth(), mSaveArea.getHeight(), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(saveBitmap);
+                Paint paint = new Paint();
+                canvas.drawBitmap(bmp, new Rect(mCutLeft, mCutTop, mCutLeft + mSaveArea.getWidth(), mCutTop + mSaveArea.getHeight()),
+                        new Rect(0, 0, mSaveArea.getWidth(), mSaveArea.getHeight()), paint);
+
+                File imageDir = new File(Constant.IMAGE_DIR);
+                if (!imageDir.exists()) {
+                    imageDir.mkdir();
+                }
+                String imageName = Constant.SCREEN_SHOT;
+                File file = new File(imageDir, imageName);
+                try {
+                    if (file.exists()) {
+                        file.delete();
+                    }
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                FileOutputStream os = new FileOutputStream(file);
+                saveBitmap.compress(Bitmap.CompressFormat.PNG, 100, os);
+                os.flush();
+                os.close();
+
+                //将截图保存至相册并广播通知系统刷新
+                MediaStore.Images.Media.insertImage(getContentResolver(), file.getAbsolutePath(), imageName, null);
+                Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(file));
+                sendBroadcast(intent);
+                Toast.makeText(TakePhotosActivity.this, "保存成功", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(TakePhotosActivity.this, "保存失败", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 }
---
+++ app/src/main/java/com/lj/apps/login/utils/Constant.java
@@ -0,0 +1,14 @@
+package com.lj.apps.login.utils;
+
+import android.os.Environment;
+
+import java.io.File;
+
+/**
+ * Created by LXG on 2016/12/27.
+ */
+
+public class Constant {
+    public static final String IMAGE_DIR = Environment.getExternalStorageDirectory() + File.separator + "Android截屏";
+    public static final String SCREEN_SHOT = "screenshot.png";
+}
